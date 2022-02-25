316af1d54272c0f50b5607c922e9b8869f38aa06
MoonisTran <96872572+MoonisTran@users.noreply.github.com>
2022-02-25 09:39:22 +0700

    test-cauA

---
+++ test-A.js
@@ -0,0 +1,27 @@
+//A1
+
+function findOppositeNumber(n, firstNumber) {
+	return (n / 2 + firstNumber) % n;
+}
+
+//A2
+
+function merge (s1, s2) {
+
+    let a = s1.split("").filter(Boolean);
+  
+    let b = s2.split("");
+  
+    let mergedString = '';
+  
+  
+    for(let i = 0; i < a.length || i < b.length; i++) {  
+     if(i < a.length) 
+       mergedString +=  a[i];
+     if(i < b.length) 
+       mergedString +=  b[i];
+    }
+  return mergedString;
+  
+  }
+  console.log(merge('abc','123'));
